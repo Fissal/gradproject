cb8be008e3cb87cda0763fdb292dfb0f2706e29a
packnowledge <fissalalsharef@gmail.com>
2017-01-25 19:31:57 +0300

    A new Model has been added.

--- video_Pro.py
+++ video_Pro.py
@@ -75,21 +75,42 @@
 
 import numpy as np
 import cv2
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-img = cv2.imread('im1.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('cascade.xml')
+cap = cv2.VideoCapture(0)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv2.imshow('img',img)
-cv2.waitKey(0)
+while (True):
+    ret, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # print gray
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print faces
+    # if (len(faces) != 0):
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0,0,255), 2)
+
+    cv2.imshow('frame', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
+
+#
+
+# import cv2
+#
+# vidcap = cv2.VideoCapture('vtest.mp4')
+# vidcap.set(cv2.CAP_PROP_POS_MSEC,20000)      # just cue to 20 sec. position
+# success,image = vidcap.read()
+# if success:
+#     print "s"
+#     cv2.imwrite("frame20sec.jpg", image)     # save frame as JPEG file
+#     cv2.imshow("20sec",image)
+#     cv2.waitKey()
+
+
+
 
